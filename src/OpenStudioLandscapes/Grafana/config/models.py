4fb7317c1b3d243698af6414c0b77714f5acc185
Michael Mussato <michimussato@gmail.com>
2025-12-17 14:31:29 +0100

    47-refactor-to-config-based-landscapes: OpenStudioLandscapes-Grafana updated

---
+++ src/OpenStudioLandscapes/Grafana/config/models.py
@@ -0,0 +1,72 @@
+import enum
+import pathlib
+
+from dagster import get_dagster_logger
+from pydantic import (
+    Field,
+    PositiveInt,
+)
+
+LOGGER = get_dagster_logger(__name__)
+
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
+
+from OpenStudioLandscapes.Grafana import dist
+
+config_default = pathlib.Path(__file__).parent.joinpath("config_default.yml")
+CONFIG_STR = config_default.read_text()
+
+
+class GrafanaDockerImage(enum.StrEnum):
+    enterprise = "docker.io/grafana/grafana-enterprise"
+    oss_legacy = "docker.io/grafana/grafana-oss"
+    oss = "docker.io/grafana/grafana"
+
+
+class GrafanaDockerImageVersion(enum.StrEnum):
+    latest = "latest"
+    latest_ubuntu = "latest-ubuntu"
+    main = "main"
+    main_ubuntu = "main-ubuntu"
+    version_11_6 = "11.6"
+    version_11_6_ubuntu = "11.6-ubuntu"
+
+
+class Config(FeatureBaseModel):
+
+    feature_name: str = dist.name
+
+    definitions: str = "OpenStudioLandscapes.Ayon.definitions"
+
+    grafana_admin_user: str = Field(
+        default="openstudiolandscapes",
+        description="The Grafana Admin username.",
+        frozen=True,
+    )
+
+    grafana_admin_password: str = Field(
+        default="openstudiolandscapes",
+        description="The Grafana Admin password.",
+        frozen=True,
+    )
+
+    grafana_port_container: PositiveInt = Field(
+        default=3000,
+        description="The Grafana container port.",
+        frozen=True,
+    )
+    grafana_port_host: PositiveInt = Field(
+        default=3030,
+        description="The Grafana host port.",
+        frozen=False,
+    )
+
+    grafana_image: GrafanaDockerImage = Field(
+        default=GrafanaDockerImage.oss,
+        examples=[i.name for i in GrafanaDockerImage],
+    )
+
+    grafana_image_version: GrafanaDockerImageVersion = Field(
+        default=GrafanaDockerImageVersion.latest_ubuntu,
+        examples=[i.name for i in GrafanaDockerImageVersion],
+    )
--- src/OpenStudioLandscapes/Grafana/constants.py
+++ src/OpenStudioLandscapes/Grafana/constants.py
@@ -1,149 +1,15 @@
 __all__ = [
-    "DOCKER_USE_CACHE",
     "ASSET_HEADER",
-    "FEATURE_CONFIGS",
 ]
 
-import pathlib
-from typing import Any, Generator
+from OpenStudioLandscapes.Grafana import dist
 
-from dagster import (
-    AssetExecutionContext,
-    AssetMaterialization,
-    AssetOut,
-    MetadataValue,
-    Output,
-    get_dagster_logger,
-    multi_asset,
-)
-
-LOGGER = get_dagster_logger(__name__)
-
-from OpenStudioLandscapes.engine.constants import DOCKER_USE_CACHE_GLOBAL
-from OpenStudioLandscapes.engine.enums import OpenStudioLandscapesConfig
-
-DOCKER_USE_CACHE = DOCKER_USE_CACHE_GLOBAL or False
-
-
-GROUP = "Grafana"
+# Todo
+#  - [ ] fix this naive replacement logic
+GROUP = dist.name.replace("-", "_")
 KEY = [GROUP]
-FEATURE = f"OpenStudioLandscapes-{GROUP}".replace("_", "-")
 
 ASSET_HEADER = {
     "group_name": GROUP,
     "key_prefix": KEY,
 }
-
-# @formatter:off
-FEATURE_CONFIGS = {
-    OpenStudioLandscapesConfig.DEFAULT: {
-        "DOCKER_USE_CACHE": DOCKER_USE_CACHE,
-        # "HOSTNAME": "grafana",
-        "GRAFANA_ADMIN_USER": "openstudiolandscapes",
-        "GRAFANA_ADMIN_PASSWORD": "openstudiolandscapes",
-        "GRAFANA_PORT_HOST": "3030",
-        "GRAFANA_PORT_CONTAINER": "3000",
-        # https://grafana.com/docs/grafana/latest/setup-grafana/installation/docker/
-        "GRAFANA_DOCKER_IMAGE": {
-            "enterprise": "docker.io/grafana/grafana-enterprise",
-            "oss_legacy": "docker.io/grafana/grafana-oss",
-            "oss": "docker.io/grafana/grafana",
-        }["oss"],
-        "GRAFANA_DOCKER_IMAGE_VERSION": {
-            "latest": "latest",
-            "latest-ubuntu": "latest-ubuntu",
-            "main": "main",
-            "main-ubuntu": "main-ubuntu",
-            "11.6": "11.6",
-            "11.6-ubuntu": "11.6-ubuntu",
-        }["latest-ubuntu"],
-    }
-}
-# @formatter:on
-
-
-# Todo:
-#  - [ ] move to common_assets
-@multi_asset(
-    name=f"constants_{GROUP}",
-    outs={
-        "NAME": AssetOut(
-            **ASSET_HEADER,
-            dagster_type=str,
-            description="",
-        ),
-        "FEATURE_CONFIGS": AssetOut(
-            **ASSET_HEADER,
-            dagster_type=dict,
-            description="",
-        ),
-        "DOCKER_COMPOSE": AssetOut(
-            **ASSET_HEADER,
-            dagster_type=pathlib.Path,
-            description="",
-        ),
-    },
-)
-def constants_multi_asset(
-    context: AssetExecutionContext,
-) -> Generator[
-    Output[dict[OpenStudioLandscapesConfig, dict[str, bool | str]]]
-    | AssetMaterialization
-    | Output[Any]
-    | Output[pathlib.Path]
-    | Any,
-    None,
-    None,
-]:
-    """ """
-
-    yield Output(
-        output_name="FEATURE_CONFIGS",
-        value=FEATURE_CONFIGS,
-    )
-
-    yield AssetMaterialization(
-        asset_key=context.asset_key_for_output("FEATURE_CONFIGS"),
-        metadata={
-            "__".join(
-                context.asset_key_for_output("FEATURE_CONFIGS").path
-            ): MetadataValue.json(FEATURE_CONFIGS),
-        },
-    )
-
-    yield Output(
-        output_name="NAME",
-        value=__name__,
-    )
-
-    yield AssetMaterialization(
-        asset_key=context.asset_key_for_output("NAME"),
-        metadata={
-            "__".join(context.asset_key_for_output("NAME").path): MetadataValue.path(
-                __name__
-            ),
-        },
-    )
-
-    docker_compose = pathlib.Path(
-        "{DOT_LANDSCAPES}",
-        "{LANDSCAPE}",
-        f"{ASSET_HEADER['group_name']}__{'_'.join(ASSET_HEADER['key_prefix'])}",
-        "__".join(context.asset_key_for_output("DOCKER_COMPOSE").path),
-        "docker_compose",
-        "docker-compose.yml",
-    )
-
-    yield Output(
-        output_name="DOCKER_COMPOSE",
-        value=docker_compose,
-    )
-
-    yield AssetMaterialization(
-        asset_key=context.asset_key_for_output("DOCKER_COMPOSE"),
-        metadata={
-            "__".join(
-                context.asset_key_for_output("DOCKER_COMPOSE").path
-            ): MetadataValue.path(docker_compose),
-        },
-    )
