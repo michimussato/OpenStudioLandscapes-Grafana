f9973823de48119be63cd8453a4990670701ab7a
Michael Mussato <michimussato@gmail.com>
2026-04-26 21:07:49 +0200

    remove compute_fields. serializing them causes problems and is also not needed.

--- src/OpenStudioLandscapes/Grafana/config/models.py
+++ src/OpenStudioLandscapes/Grafana/config/models.py
@@ -8,7 +8,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
-    computed_field,
+    # computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -965,7 +965,7 @@ class Config(FeatureBaseModel):
     #     return ret
 
     # SUBSTITUTED TEMPLATE
-    @computed_field
+    # @computed_field
     @property
     def alloy_config(self) -> str:
         template = AlloyConfigTemplate(self.alloy_config_template)
@@ -982,7 +982,7 @@ class Config(FeatureBaseModel):
         return ret
 
     # EXPANDABLE PATHS
-    @computed_field
+    # @computed_field
     @property
     def GF_PATHS_DATA_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
