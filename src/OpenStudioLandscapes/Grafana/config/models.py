dffaa779531f22feca18515d0035f0c668fa37a0
Michael Mussato <michimussato@gmail.com>
2026-05-02 13:11:30 +0200

    add better logging

--- src/OpenStudioLandscapes/Grafana/config/models.py
+++ src/OpenStudioLandscapes/Grafana/config/models.py
@@ -4,16 +4,12 @@ import textwrap
 from string import Template
 from typing import Dict, List, Union
 
-from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
 )
 
-from OpenStudioLandscapes.cli import LOGGING_LEVEL_DEFAULT
-
-LOGGER = get_dagster_logger(__name__)
-LOGGER.setLevel(LOGGING_LEVEL_DEFAULT)
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
