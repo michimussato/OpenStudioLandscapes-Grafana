38b736b35d44424f5c1169e41c4413ef43518f9b
Michael Mussato <michimussato@gmail.com>
2026-05-12 13:46:38 +0200

    remove missing import

--- src/OpenStudioLandscapes/Grafana/_definitions_with_upstream_specs.py
+++ src/OpenStudioLandscapes/Grafana/_definitions_with_upstream_specs.py
@@ -1,7 +1,7 @@
 from dagster import Definitions
 from OpenStudioLandscapes.engine.base.assets import group_out_base
 
-from OpenStudioLandscapes.Grafana.definitions import assets_base, constants_base
+from OpenStudioLandscapes.Grafana.definitions import assets_base
 
 assets_external = []
 assets_external.extend(group_out_base.specs)
@@ -10,7 +10,6 @@ assets_external.extend(group_out_base.specs)
 defs = Definitions(
     assets=[
         *assets_base,
-        *constants_base,
         *assets_external,
     ],
 )
